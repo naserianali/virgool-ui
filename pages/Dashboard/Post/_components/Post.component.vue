fc33f37c82cc02e961e7b61c14a20debed74ec04
CyberALi1989 <naserianali@gmail.com>
2025-02-10 23:13:37 +0330

    remove log

--- pages/Dashboard/Post/_components/Post.component.vue
+++ pages/Dashboard/Post/_components/Post.component.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import type {IBlogs} from "~/pages/Dashboard/Post/_components/Posts.component.vue";
+import {useBlogStore} from "~/store/blog.store";
 
 const {blog} = defineProps({
   blog: {
     required: true,
     type: Object as PropType<IBlogs>
   }
-})
+});
+const store = useBlogStore()
+const setBlogUpdateData = () => {
+  store.setData(blog)
+}
 </script>
 
 <template>
-  <NuxtLink to="/" class="flex flex-col rounded-xl overflow-hidden p-2 shadow mb-6 mx-auto">
+  <NuxtLink :to="{name: 'Dashboard-Post-Form'}"
+            v-on:click="setBlogUpdateData"
+            class="flex flex-col rounded-xl overflow-hidden p-2 shadow mb-6 mx-auto">
     <div class="relative">
       <img class="size-44 w-full object-cover rounded-xl" :src="blog.image.src" :alt="blog.image.alt">
       <p class="flex items-center justify-between w-fit gap-8 text-white p-2 rounded-xl absolute bottom-0 bg-black/40">
